9353e821d75a7c432cc2006f929a802957b2bd8c
Evgeniy Rybkin <helloevgeniyrybkin@gmail.com>
2023-10-11 20:45:14 +0300

    Added error handling during telegram api expose

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -4,17 +4,25 @@ declare(strict_types=1);
 
 namespace App\Services\Telegram;
 
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class TelegramBotApi
 {
     public const HOST = 'https://api.telegram.org/bot';
 
-    public static function sendMessage(string $token, int $chatId, string $message): void
+    public static function sendMessage(string $token, int $chatId, string $message): bool
     {
-        Http::get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chatId,
-            'text' => $message,
-        ]);
+        $url = sprintf('%s%s/sendMessage', self::HOST, $token);
+
+        $query = ['chat_id' => $chatId, 'text' => $message];
+
+        try {
+            Http::get($url, $query);
+
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
     }
 }

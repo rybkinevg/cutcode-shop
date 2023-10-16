426822fc7b0b1afbae020a760f12132293273ba0
Evgeniy Rybkin <helloevgeniyrybkin@gmail.com>
2023-10-16 21:13:14 +0300

    Implemented custom faker image provider

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -53,6 +53,12 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+
+        'tests' => [
+            'driver' => 'local',
+            'root' => base_path('tests'),
             'throw' => false,
         ],
 

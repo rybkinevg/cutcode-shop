17631c5614aa3f8d006caab78950f5bb7916fee6
Evgeniy Rybkin <helloevgeniyrybkin@gmail.com>
2023-10-16 17:27:45 +0300

    Created migration, factory, seeder and model for the Product entity

---
+++ app/Models/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'thumbnail',
+        'price',
+        'brand_id',
+    ];
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        // TODO: will be refactored
+        static::creating(function (Product $product) {
+            $product->slug = $product->slug ?? str($product->title)->slug();
+        });
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
---
+++ database/migrations/2023_10_16_141426_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('thumbnail')->nullable();
+            $table->unsignedInteger('price')->default(0);
+            $table->foreignIdFor(Brand::class)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Category::class)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignIdFor(Product::class)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        // TODO: will be refactored via stubs
+        if (!app()->isLocal()) {
+            return;
+        }
+
+        Schema::dropIfExists('category_product');
+        Schema::dropIfExists('products');
+    }
+};

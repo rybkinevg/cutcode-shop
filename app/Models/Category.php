5a1c2a4f6cec87008b5c61e93de31ee7609fb522
Evgeniy Rybkin <helloevgeniyrybkin@gmail.com>
2023-10-16 17:13:03 +0300

    Created migration, factory, seeder and model for the Category entity

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'slug',
+        'title',
+    ];
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        // TODO: will be refactored
+        static::creating(function (Category $category) {
+            $category->slug = $category->slug ?? str($category->title)->slug();
+        });
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,5 +11,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Brand::factory(20)->create();
+        Category::factory(20)->create();
     }
 }
